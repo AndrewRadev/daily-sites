5352b6651e188edcff3d8946d4462314ecfdba04
Andrew Radev <andrey.radev@gmail.com>
2011-04-30 22:56:03 +0300

    Site day names

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,9 +1,23 @@
 class Site < ActiveRecord::Base
-  Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday = [1, 2, 3, 4, 5, 6, 7]
+  Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday = (1 .. 7).to_a
+
+  DayNames = {
+    Monday    => 'monday',
+    Tuesday   => 'tuesday',
+    Wednesday => 'wednesday',
+    Thursday  => 'thursday',
+    Friday    => 'friday',
+    Saturday  => 'saturday',
+    Sunday    => 'sunday',
+  }
 
   serialize :days
 
   def days
     super or Set.new
+  end
+
+  def day_names
+    days.to_a.sort.map { |d| DayNames[d] }.to_sentence
   end
 end
--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,17 +1,29 @@
 require 'spec_helper'
 
 describe Site do
-  it "keeps the days it's active in an array" do
-    site = Site.sample
+  let(:site) { Site.sample }
 
+  it "keeps the days it's active in a set" do
+    site.days = Set.new
     site.days << Site::Wednesday
     site.days << Site::Monday
     site.days << Site::Friday
 
     site.save!
-
     site.reload
 
     site.days.should eq [Site::Monday, Site::Wednesday, Site::Friday].to_set
+  end
+
+  it "displays the days it's active as a sentence" do
+    site.days = Set.new
+    site.days << Site::Friday
+    site.days << Site::Tuesday
+    site.days << Site::Saturday
+
+    site.save!
+    site.reload
+
+    site.day_names.should eq 'tuesday, friday, and saturday'
   end
 end
