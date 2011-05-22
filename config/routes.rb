e6aa3a4e75d15dfd7aed5f81d6e88948050a0874
Andrew Radev <andrey.radev@gmail.com>
2011-05-22 22:22:27 +0300

    Initial omniauth twitter integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 DailySites::Application.routes.draw do
   root :to => 'sites#index'
 
+  get '/auth/:provider/callback' => 'sessions#create'
+
   resources :sites, :except => [:show] do
     collection do
       get :all
