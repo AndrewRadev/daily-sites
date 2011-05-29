31e5c76d3627f380a2233dd3c95ab62707887cc9
Andrew Radev <andrey.radev@gmail.com>
2011-05-29 23:15:00 +0300

    Signout

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -27,4 +27,17 @@ describe SessionsController do
       session[:user_id].should eq user.id
     end
   end
+
+  describe "destroy" do
+    it "logs the current user out" do
+      session[:user_id] = '123'
+      delete :destroy
+      session[:user_id].should be_nil
+    end
+
+    it "redirects to the about page" do
+      delete :destroy
+      response.should redirect_to about_page_path
+    end
+  end
 end
