da11b9c8e73c8f184b27ebbd5d4655b5ce69403a
Andrew Radev <andrey.radev@gmail.com>
2011-06-21 22:09:10 +0300

    Facebook + Heroku, take 2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter,  ENV['TWITTER_KEY'],     ENV['TWITTER_SECRET']
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], {
     :client_options => {
-      :ssl => { :ca_path => '/usr/lib/ssl/certs/ca-certificates.crt' }
+      :ssl => {
+        :ca_path => '/usr/lib/ssl/certs'
+        :ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'
+      }
     }
   }
 end
