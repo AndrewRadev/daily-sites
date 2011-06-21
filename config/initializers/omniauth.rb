025ec06fe290a24905a8c96af15004bcfd32b621
Andrew Radev <andrey.radev@gmail.com>
2011-06-21 22:07:20 +0300

    Trying to get facebook integration working on Heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  provider :twitter,  ENV['TWITTER_KEY'],     ENV['TWITTER_SECRET']
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], {
+    :client_options => {
+      :ssl => { :ca_path => '/usr/lib/ssl/certs/ca-certificates.crt' }
+    }
+  }
 end
